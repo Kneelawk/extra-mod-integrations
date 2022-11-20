f3f4c04df5f0d34511ab9f8e2f00a4a11a6b3e2d
Kneelawk <kneelawk@gmail.com>
2022-11-19 19:30:52 -0800

    Initial project setup

---
+++ src/main/java/com/kneelawk/extramodintegrations/ExtraModIntegrationsMod.java
@@ -0,0 +1,14 @@
+package com.kneelawk.extramodintegrations;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ExtraModIntegrationsMod implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("extra-mod-integrations");
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
--- src/main/java/com/kneelawk/extramodintegrations/mixin/ExampleMixin.java
+++ src/main/java/com/kneelawk/extramodintegrations/mixin/ExampleMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package com.kneelawk.extramodintegrations.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import com.kneelawk.extramodintegrations.ExtraModIntegrationsMod;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		ExtraModIntegrationsMod.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
