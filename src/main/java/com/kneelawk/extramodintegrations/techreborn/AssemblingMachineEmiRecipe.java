5d0d6f77f102d82c9a834e05c854be971798543e
Kneelawk <kneelawk@gmail.com>
2022-11-21 22:34:08 -0800

    Add assembling machine support

---
+++ src/main/java/com/kneelawk/extramodintegrations/techreborn/AssemblingMachineEmiRecipe.java
@@ -0,0 +1,39 @@
+package com.kneelawk.extramodintegrations.techreborn;
+
+import com.kneelawk.extramodintegrations.util.UIUtils;
+import dev.emi.emi.api.recipe.EmiRecipeCategory;
+import dev.emi.emi.api.widget.WidgetHolder;
+import reborncore.common.crafting.RebornRecipe;
+
+public class AssemblingMachineEmiRecipe extends TREmiRecipe {
+    protected AssemblingMachineEmiRecipe(RebornRecipe recipe) {
+        super(recipe);
+        checkOutputCount(1);
+    }
+
+    @Override
+    public EmiRecipeCategory getCategory() {
+        return TRIntegrationImpl.ASSEMBLING_MACHINE_CATEGORY;
+    }
+
+    @Override
+    public int getDisplayWidth() {
+        return 16 + 18 + 24 + 26;
+    }
+
+    @Override
+    public int getDisplayHeight() {
+        return 50;
+    }
+
+    @Override
+    public void addWidgets(WidgetHolder widgets) {
+        widgets.addSlot(getInput(0), 16, (50 - 18 * 2 - 2) / 2);
+        widgets.addSlot(getInput(1), 16, (50 - 18 * 2 - 2) / 2 + 18 + 2);
+        widgets.addSlot(getOutput(0), 16 + 18 + 24, (50 - 26) / 2).output(true).recipeContext(this);
+
+        TRUIUtils.energyBar(widgets, recipe, 0, 0);
+        TRUIUtils.arrowRight(widgets, recipe, 16 + 18 + 4, (50 - 10) / 2);
+        UIUtils.cookTime(widgets, recipe.getTime(), 16 + 18 + 2, 0);
+    }
+}
--- src/main/java/com/kneelawk/extramodintegrations/techreborn/TRIntegrationImpl.java
+++ src/main/java/com/kneelawk/extramodintegrations/techreborn/TRIntegrationImpl.java
@@ -16,11 +16,15 @@ import static com.kneelawk.extramodintegrations.ExMIPlugin.SIMPLIFIED_ICONS;
 public class TRIntegrationImpl extends TRIntegration {
     public static final EmiStack ALLOY_SMELTER_STACK = EmiStack.of(TRContent.Machine.ALLOY_SMELTER);
     public static final EmiStack IRON_ALLOY_FURNACE_STACK = EmiStack.of(TRContent.Machine.IRON_ALLOY_FURNACE);
+    public static final EmiStack ASSEMBLY_MACHINE_STACK = EmiStack.of(TRContent.Machine.ASSEMBLY_MACHINE);
     public static final EmiStack GRINDER_STACK = EmiStack.of(TRContent.Machine.GRINDER);
 
     public static final EmiRecipeCategory ALLOY_SMELTER_CATEGORY =
         new EmiRecipeCategory(new Identifier("techreborn:alloy_smelter"), ALLOY_SMELTER_STACK,
             new EmiTexture(SIMPLIFIED_ICONS, 16, 0, 16, 16));
+    public static final EmiRecipeCategory ASSEMBLING_MACHINE_CATEGORY =
+        new EmiRecipeCategory(new Identifier("techreborn:assembling_machine"), ASSEMBLY_MACHINE_STACK,
+            new EmiTexture(SIMPLIFIED_ICONS, 32, 0, 16, 16));
     public static final EmiRecipeCategory GRINDER_CATEGORY =
         new EmiRecipeCategory(new Identifier("techreborn:grinder"), GRINDER_STACK,
             new EmiTexture(SIMPLIFIED_ICONS, 0, 0, 16, 16));
@@ -35,6 +39,13 @@ public class TRIntegrationImpl extends TRIntegration {
         registry.addWorkstation(ALLOY_SMELTER_CATEGORY, IRON_ALLOY_FURNACE_STACK);
         for (RebornRecipe recipe : registry.getRecipeManager().listAllOfType(ModRecipes.ALLOY_SMELTER)) {
             registry.addRecipe(new AlloySmelterEmiRecipe(recipe));
+        }
+
+        // Assembling
+        registry.addCategory(ASSEMBLING_MACHINE_CATEGORY);
+        registry.addWorkstation(ASSEMBLING_MACHINE_CATEGORY, ASSEMBLY_MACHINE_STACK);
+        for (RebornRecipe recipe : registry.getRecipeManager().listAllOfType(ModRecipes.ASSEMBLING_MACHINE)) {
+            registry.addRecipe(new AssemblingMachineEmiRecipe(recipe));
         }
 
         // Grinding
