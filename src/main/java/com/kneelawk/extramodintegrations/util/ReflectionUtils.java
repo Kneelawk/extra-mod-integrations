a1da2677ecf62c16d0ac4b334131b68f08a8fd2b
Kneelawk <kneelawk@gmail.com>
2023-04-20 22:32:07 -0700

    Clean up abstract integration

--- src/main/java/com/kneelawk/extramodintegrations/util/ReflectionUtils.java
+++ src/main/java/com/kneelawk/extramodintegrations/util/ReflectionUtils.java
@@ -2,6 +2,10 @@ package com.kneelawk.extramodintegrations.util;
 
 import java.lang.reflect.InvocationTargetException;
 
+import org.jetbrains.annotations.Nullable;
+
+import com.kneelawk.extramodintegrations.ExMIMod;
+
 public class ReflectionUtils {
     @SuppressWarnings("unchecked")
     public static <T> T newInstance(String name) throws ClassNotFoundException {
@@ -9,6 +13,17 @@ public class ReflectionUtils {
             return (T) Class.forName(name).getConstructor().newInstance();
         } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
             throw new RuntimeException("Unable to get instance: " + name, e);
+        }
+    }
+
+    public static <T> @Nullable T newIntegrationInstance(String name, String modDisplayName) {
+        try {
+            return ReflectionUtils.newInstance(name);
+        } catch (ClassNotFoundException e) {
+            ExMIMod.LOGGER.warn("[Extra Mod Integrations] Attempted to load " + modDisplayName + " integration, but " +
+                "Extra Mod Integrations was compiled with " + modDisplayName + " integration disabled. " +
+                "EMI <-> " + modDisplayName + " integration will not work.");
+            return null;
         }
     }
 }
