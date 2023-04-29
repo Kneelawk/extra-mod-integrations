cd6809cbe4d1f6d124f75744ea08cb6f6feaae5c
Kneelawk <kneelawk@gmail.com>
2023-04-29 14:10:40 -0700

    Add module crafting

--- src/main/java/com/kneelawk/extramodintegrations/indrev/IRIntegration.java
+++ src/main/java/com/kneelawk/extramodintegrations/indrev/IRIntegration.java
@@ -6,9 +6,16 @@ import dev.emi.emi.api.EmiRegistry;
 import dev.emi.emi.api.recipe.EmiRecipeCategory;
 import dev.emi.emi.api.recipe.EmiRecipeSorting;
 import dev.emi.emi.api.stack.EmiStack;
-import me.steven.indrev.recipes.machines.*;
+import me.steven.indrev.recipes.machines.CompressorRecipe;
+import me.steven.indrev.recipes.machines.CondenserRecipe;
+import me.steven.indrev.recipes.machines.ElectrolysisRecipe;
+import me.steven.indrev.recipes.machines.InfuserRecipe;
+import me.steven.indrev.recipes.machines.ModuleRecipe;
+import me.steven.indrev.recipes.machines.PulverizerRecipe;
+import me.steven.indrev.recipes.machines.SawmillRecipe;
 import me.steven.indrev.registry.MachineRegistry;
 
+import net.minecraft.recipe.RecipeManager;
 import net.minecraft.util.Identifier;
 
 import com.kneelawk.extramodintegrations.AbstractIRIntegration;
@@ -21,10 +28,14 @@ public class IRIntegration extends AbstractIRIntegration {
     public static final EmiStack[] COMPRESSOR_FACTORY_STACKS =
         getAllTiers(MachineRegistry.Companion.getCOMPRESSOR_FACTORY_REGISTRY());
     public static final EmiStack[] CONDENSER_STACKS = getAllTiers(MachineRegistry.Companion.getCONDENSER_REGISTRY());
-    public static final EmiStack[] ELECTROLYSIS_STACKS = getAllTiers(MachineRegistry.Companion.getELECTROLYTIC_SEPARATOR_REGISTRY());
-    public static final EmiStack[] SOLID_INFUSER_STACKS = getAllTiers(MachineRegistry.Companion.getSOLID_INFUSER_REGISTRY());
+    public static final EmiStack[] ELECTROLYSIS_STACKS =
+        getAllTiers(MachineRegistry.Companion.getELECTROLYTIC_SEPARATOR_REGISTRY());
+    public static final EmiStack[] SOLID_INFUSER_STACKS =
+        getAllTiers(MachineRegistry.Companion.getSOLID_INFUSER_REGISTRY());
     public static final EmiStack[] PULVERIZER_STACKS = getAllTiers(MachineRegistry.Companion.getPULVERIZER_REGISTRY());
     public static final EmiStack[] SAWMILL_STACKS = getAllTiers(MachineRegistry.Companion.getSAWMILL_REGISTRY());
+    public static final EmiStack[] MODULAR_WORKBENCH_STACKS =
+        getAllTiers(MachineRegistry.Companion.getMODULAR_WORKBENCH_REGISTRY());
 
     public static final EmiRecipeCategory COMPRESS_CATEGORY =
         new EmiRecipeCategory(irId("compress"), COMPRESSOR_STACKS[0], ExMITextures.COMPRESSING,
@@ -42,6 +53,10 @@ public class IRIntegration extends AbstractIRIntegration {
         new EmiRecipeCategory(irId("infuse"), SOLID_INFUSER_STACKS[0], ExMITextures.CHEMICAL_REACTING,
             EmiRecipeSorting.compareOutputThenInput());
 
+    public static final EmiRecipeCategory MODULES_CATEGORY =
+        new EmiRecipeCategory(irId("modules"), MODULAR_WORKBENCH_STACKS[0], ExMITextures.MODULE,
+            EmiRecipeSorting.compareOutputThenInput());
+
     public static final EmiRecipeCategory PULVERIZE_CATEGORY =
         new EmiRecipeCategory(irId("pulverize"), PULVERIZER_STACKS[0], ExMITextures.GRINDING,
             EmiRecipeSorting.compareOutputThenInput());
@@ -53,48 +68,55 @@ public class IRIntegration extends AbstractIRIntegration {
     @Override
     protected void registerImpl(EmiRegistry registry) {
         ExMIMod.LOGGER.info("[Extra Mod Integrations] Loading Industrial Revolution Integration...");
+        RecipeManager manager = registry.getRecipeManager();
 
         // Compressor
         registry.addCategory(COMPRESS_CATEGORY);
         for (EmiStack stack : COMPRESSOR_STACKS) registry.addWorkstation(COMPRESS_CATEGORY, stack);
         for (EmiStack stack : COMPRESSOR_FACTORY_STACKS) registry.addWorkstation(COMPRESS_CATEGORY, stack);
-        for (CompressorRecipe recipe : registry.getRecipeManager()
-            .listAllOfType(CompressorRecipe.Companion.getTYPE())) {
+        for (CompressorRecipe recipe : manager.listAllOfType(CompressorRecipe.Companion.getTYPE())) {
             registry.addRecipe(new SimpleOneInputEmiRecipe(recipe, COMPRESS_CATEGORY));
         }
 
         // Condenser
         registry.addCategory(CONDENSER_CATEGORY);
         for (EmiStack stack : CONDENSER_STACKS) registry.addWorkstation(CONDENSER_CATEGORY, stack);
-        for (CondenserRecipe recipe : registry.getRecipeManager().listAllOfType(CondenserRecipe.Companion.getTYPE())) {
+        for (CondenserRecipe recipe : manager.listAllOfType(CondenserRecipe.Companion.getTYPE())) {
             registry.addRecipe(new CondenserEmiRecipe(recipe));
         }
 
         // Electrolysis
         registry.addCategory(ELECTROLYSIS_CATEGORY);
         for (EmiStack stack : ELECTROLYSIS_STACKS) registry.addWorkstation(ELECTROLYSIS_CATEGORY, stack);
-        for (ElectrolysisRecipe recipe: registry.getRecipeManager().listAllOfType(ElectrolysisRecipe.Companion.getTYPE())) {
+        for (ElectrolysisRecipe recipe : manager.listAllOfType(ElectrolysisRecipe.Companion.getTYPE())) {
             registry.addRecipe(new ElectrolysisEmiRecipe(recipe));
         }
 
         // Solid Infuser
         registry.addCategory(INFUSE_CATEGORY);
         for (EmiStack stack : SOLID_INFUSER_STACKS) registry.addWorkstation(INFUSE_CATEGORY, stack);
-        for (InfuserRecipe recipe : registry.getRecipeManager().listAllOfType(InfuserRecipe.Companion.getTYPE())) {
+        for (InfuserRecipe recipe : manager.listAllOfType(InfuserRecipe.Companion.getTYPE())) {
             registry.addRecipe(new SimpleTwoInputEmiRecipe(recipe, INFUSE_CATEGORY));
+        }
+
+        // Modular Workbench
+        registry.addCategory(MODULES_CATEGORY);
+        for (EmiStack stack : MODULAR_WORKBENCH_STACKS) registry.addWorkstation(MODULES_CATEGORY, stack);
+        for (ModuleRecipe recipe : manager.listAllOfType(ModuleRecipe.Companion.getTYPE())) {
+            registry.addRecipe(new ModuleEmiRecipe(recipe));
         }
 
         // Pulverizer
         registry.addCategory(PULVERIZE_CATEGORY);
         for (EmiStack stack : PULVERIZER_STACKS) registry.addWorkstation(PULVERIZE_CATEGORY, stack);
-        for (PulverizerRecipe recipe : registry.getRecipeManager().listAllOfType(PulverizerRecipe.Companion.getTYPE())) {
+        for (PulverizerRecipe recipe : manager.listAllOfType(PulverizerRecipe.Companion.getTYPE())) {
             registry.addRecipe(new PulverizerEmiRecipe(recipe));
         }
 
         // Sawmill
         registry.addCategory(SAWMILL_CATEGORY);
         for (EmiStack stack : SAWMILL_STACKS) registry.addWorkstation(SAWMILL_CATEGORY, stack);
-        for (SawmillRecipe recipe : registry.getRecipeManager().listAllOfType(SawmillRecipe.Companion.getTYPE())) {
+        for (SawmillRecipe recipe : manager.listAllOfType(SawmillRecipe.Companion.getTYPE())) {
             registry.addRecipe(new SawmillEmiRecipe(recipe));
         }
     }
