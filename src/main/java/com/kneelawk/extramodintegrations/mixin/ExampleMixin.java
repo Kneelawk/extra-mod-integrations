bb58ba855ea4ea2c306c1a0ead7c27cc7279b771
Kneelawk <kneelawk@gmail.com>
2023-08-29 22:02:58 -0700

    Disable example mixin message

--- src/main/java/com/kneelawk/extramodintegrations/mixin/ExampleMixin.java
+++ src/main/java/com/kneelawk/extramodintegrations/mixin/ExampleMixin.java
@@ -1,16 +1,16 @@
 package com.kneelawk.extramodintegrations.mixin;
 
-import com.kneelawk.extramodintegrations.ExMIMod;
-import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import net.minecraft.client.gui.screen.TitleScreen;
+
 @Mixin(TitleScreen.class)
 public class ExampleMixin {
     @Inject(at = @At("HEAD"), method = "init()V")
     private void init(CallbackInfo info) {
-        ExMIMod.LOGGER.info("This line is printed by an example mod mixin!");
+//        ExMIMod.LOGGER.info("This line is printed by an example mod mixin!");
     }
 }
