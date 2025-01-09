ae256591b025a8acf6327c31c7060b90c5dcbb1c
Cyan Kneelawk <kneelawk@gmail.com>
2025-01-09 15:43:04 -0800

    Add @EmiEntrypoint to fabric entrypoints

--- modules/tech-reborn-fabric/src/main/java/com/kneelawk/exmi/techreborn/TRPlugin.java
+++ modules/tech-reborn-fabric/src/main/java/com/kneelawk/exmi/techreborn/TRPlugin.java
@@ -1,10 +1,12 @@
 package com.kneelawk.exmi.techreborn;
 
+import dev.emi.emi.api.EmiEntrypoint;
 import dev.emi.emi.api.EmiPlugin;
 import dev.emi.emi.api.EmiRegistry;
 
 import com.kneelawk.exmi.core.api.ExMIPlugin;
 
+@EmiEntrypoint
 public class TRPlugin implements EmiPlugin {
     @Override
     public void register(EmiRegistry emiRegistry) {
